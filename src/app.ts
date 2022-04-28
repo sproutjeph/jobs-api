eb6abb7b503b5436ec58a3d9d854e97f0ce193eb
sproutjeph <don_jeph@yahoo.com>
2022-04-28 11:28:31 +0100

    fixed

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,9 @@ const app: Application = express();
 import authRouter from "./routes/auth.route";
 import jobsRouter from "./routes/jobs.route";
 import { ErrorHandlerMiddleware, NotFoundMiddleware } from "./middlewares";
-import swaggerUI from "swagger-ui-express";
-const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./swagger.yaml");
+// import swaggerUI from "swagger-ui-express";
+// const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml");
 // app.set("trust proxy", 1);
 // app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(cors());
@@ -22,7 +22,7 @@ app.use(xss());
 app.get("/", (req, res) => {
   res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMidleware, jobsRouter);
 
